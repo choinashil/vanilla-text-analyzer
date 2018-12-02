cafdb6bbadc490e9c116f7dd70da543af8d82fe7
nashil <choinashiL@gmail.com>
2018-12-02 17:10:23 +0900

    change validator from charcode to regexp

--- app/index.js
+++ app/index.js
@@ -5,32 +5,40 @@ import 'styles/index.less';
 // START YOUR APP HERE
 // ================================
 
-var inputBox = document.querySelector('#inputBox');
-var maxLimit = 5000;
 var data = {};
 var dataCount = 0;
+var maxLimit = 5000;
+var noHangeul = true;
+var lessThanLimit = true;
 
+var inputBox = document.querySelector('#inputBox');
 
 var wordCount = document.querySelector('.wordCount');
 wordCount.children[0].textContent = 'Number of characters: 0';
 wordCount.children[1].textContent = 'Total word count: 0';
-
 
 var result = document.querySelector('.result');
 
 inputBox.addEventListener('keyup', function(e) {
   validateInputValue(e);
   countLength(e);
-  analyzeInputValue(e);
 });
 
 function validateInputValue (e) {
   var allCharacters = e.target.value;
-  var currentCharacter = allCharacters[allCharacters.length - 1];
+  var regExpHangeul = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/g;
 
-  if (allCharacters) {
-    if ((currentCharacter.charCodeAt() >= 45032 && currentCharacter.charCodeAt() <= 55203) || (currentCharacter.charCodeAt() >= 12593 && currentCharacter.charCodeAt() <= 12622) || (currentCharacter.charCodeAt() >= 12623 && currentCharacter.charCodeAt() <= 12643)) {
-      alert(`${currentCharacter} is not a valid character.`);
+  if (regExpHangeul.test(allCharacters)) {
+    noHangeul = false;
+    // console.log('한글이 있습니다');
+    // console.log(noHangeul);
+    throwErrorWhenTypedHangeul();
+  } else {
+    noHangeul = true;
+    // console.log('validator 통과');
+    // console.log(noHangeul);
+    if (noHangeul && lessThanLimit) {
+      analyzeInputValue(e);
     }
   }
 }
@@ -41,21 +49,30 @@ function countLength(e) {
   wordCount.children[0].textContent = `Number of characters: ${lengthCount}`;
 
   if (lengthCount > maxLimit) {
-    alert('Exceeded maximum length.');
+    lessThanLimit = false;
+    throwErrorWhenExceededLimit(lengthCount, maxLimit);
+  } else {
+    lessThanLimit = true;
+    if (noHangeul && lessThanLimit) {
+      analyzeInputValue(e);
+    }
   }
 }
 
+var time = 100;
+var counter = 0;
+
 function analyzeInputValue(e) {
-  var allCharacters = e.target.value.toLowerCase();
-  // if (allCharacters.includes('.' || ',' || '!' || '?')) {
-  //
-  // }
+  var allCharacters = e.target.value;
+
+  var regExpSymbols = /[`~@#$%^&*\-_=+(){}\[\]<>|\\/;:'",.?!\n]/g;
+
+  if (regExpSymbols.test(allCharacters)) {
+    allCharacters = allCharacters.replace(regExpSymbols, '');
+  }
 
   if (allCharacters) {
-    var allWords = allCharacters.split(' ');
-    // allWords = allWords.map(word) {
-    //   if (word[word.length-1] === '.' || word[word.length-1] === '.' || word[word.length-1] === '.' || word[word.length-1] === '.')
-    // }
+    var allWords = allCharacters.toLowerCase().split(' ');
 
     if (allWords.includes('')) {
       var idxOfWhiteSpace = allWords.indexOf('');
@@ -67,41 +84,58 @@ function analyzeInputValue(e) {
 
     wordCount.children[1].textContent = `Total word count: ${allWords.length}`;
 
-    console.log('arr:',allWords);
-    console.log('1.',data);
-    console.log(`data.length: ${Object.keys(data).length}`);
-    console.log(`arr.length: ${allWords.length}`);
-    console.log(`dataCount: ${dataCount}`);
+    // console.log('1.',data);
+    // console.log(`dataCount: ${dataCount}`);
+
+    // console.log(`data.length: ${Object.keys(data).length}`);
+    // console.log(`arr.length: ${allWords.length}`);
+    // console.log(`dataCount: ${dataCount}`);
 
     if (e.keyCode === 32) {
       for (var i = dataCount; i < allWords.length; i++) {
-
         if (data[allWords[i]]) {
           data[allWords[i]] = data[allWords[i]] + 1;
           dataCount++;
-          console.log('2.',data);
+          // console.log('2.',data);
         } else {
           data[allWords[i]] = 1;
           dataCount++;
-          console.log('3.',data);
+          // console.log('3.',data);
         }
-
       }
       showResult();
     }
 
+  //   (function (data, allWords, dataCount) {
+  //   setTimeout(function() {
+  //     for (var i = dataCount; i < allWords.length; i++) {
+  //       if (data[allWords[i]]) {
+  //         data[allWords[i]] = data[allWords[i]] + 1;
+  //         dataCount++;
+  //         console.log('2.',data);
+  //       } else {
+  //         data[allWords[i]] = 1;
+  //         dataCount++;
+  //         console.log('3.',data);
+  //       }
+  //     }
+  //     showResult();
+  //   }, time * counter);
+  //   counter++;
+  // })(data, allWords, dataCount);
+
   } else {
     data = {};
     wordCount.children[1].textContent = 'Total word count: 0';
-
-    console.log(allWords);
-    console.log(data);
+    result.textContent = '';
+    // console.log(allWords);
+    // console.log(data);
   }
 
 }
 
 function showResult() {
-  // 글자 지웠을때 처리!
+  // 글자 지웠을때 처리하기!
   var word;
 
   for (var key in data) {
@@ -111,9 +145,7 @@ function showResult() {
       for (var i = 0; i < result.childElementCount; i++) {
         if (key === result.children[i].textContent) {
           if (data[key] > 1) {
-            result.children[i].style.fontSize = '25px';
-          } else if (data[key] > 3) {
-            result.children[i].style.fontSize = '40px';
+            changeWord(key, i);
           }
           needToCreate = false;
           break;
@@ -126,12 +158,52 @@ function showResult() {
     }
 
     if (needToCreate) {
-      word = document.createElement('span');
+      word = document.createElement('div');
       word.classList.add('word');
       word.textContent = key;
+      word.style.left = Math.floor(Math.random() * 600) + 'px';
+      word.style.top = Math.floor(Math.random() * 240) + 'px';
+      word.style.opacity = '0.4';
       result.appendChild(word);
+      // console.log(`${key}: width-${word.offsetWidth}, height-${word.offsetHeight}, left-${word.offsetLeft}, top-${word.offsetTop}`);
+
     }
 
   }
 
 }
+
+function changeWord(key, i) {
+  switch(data[key]) {
+    case 2 :
+      result.children[i].style.fontSize = '25px';
+      result.children[i].style.opacity = '0.65';
+      // console.log(`${key}: width-${result.children[i].offsetWidth}, height-${result.children[i].offsetHeight}, left-${result.children[i].offsetLeft}, top-${result.children[i].offsetTop}`);
+      break;
+    case 3 :
+      result.children[i].style.fontSize = '35px';
+      result.children[i].style.opacity = '0.75';
+      break;
+    case 4 :
+      result.children[i].style.fontSize = '45px';
+      result.children[i].style.opacity = '0.90';
+      break;
+    case 5 :
+      result.children[i].style.fontSize = '60px';
+      result.children[i].style.opacity = '1';
+      break;
+  }
+}
+
+function throwErrorWhenTypedHangeul() {
+  result.style.backgroundColor = 'orange';
+  result.textContent = '영어만 입력하세요';
+}
+
+function throwErrorWhenExceededLimit(lengthCount, maxLimit) {
+  result.style.backgroundColor = 'red';
+  result.textContent = `5000자 이하로 입력하세요. 현재 ${lengthCount - maxLimit}자 초과입니다.`;
+}
+
+// 전치사 preposition
+// [about, ]
